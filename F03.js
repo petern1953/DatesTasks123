2e2131b803adefa5fec5cad34a3051b11cbf246a
petern1953 <petern1953@gmail.com>
2020-11-30 18:14:45 +0100

    F03 ok

--- F03.js
+++ F03.js
@@ -1,17 +1,41 @@
 // Írj egy függvényt, ami paraméterként egy Date objektumot kap, a visszatérési értéke pedig egy objektum két tulajdonsággal, 
 // ami a következő formátumban tartalmazza a paraméterként kapott dátumot:
 
-// short: 2020. jan. 11. 14: 20 (tehát évszám, a hónap neve röviden magyarul, kisbetűkkel a nap, majd az idő a másodperc nélkül)
+// short: 2020. jan. 11. 14: 20 (tehát évszám, a hónap neve röviden magyarul és kisbetűkkel, a nap, majd az idő a másodperc nélkül)
 // long: 2020. január 11. 14: 20: 10 (tehát évszám, a hónap neve magyarul, a nap száma, majd az idő)
 
-const secondsFromThisYearJan01 = () => {
-    const now = new Date();
-    const thisYear = now.getFullYear();
-    const thisMonth = now.getMonth();
-    const thisDay = now.getDate();
-    const jan01InMilliseconds = Date.UTC(thisYear, 0, 1);
-    const nowInMilliseconds = Date.UTC(thisYear, thisMonth, thisDay);
-    return (nowInMilliseconds - jan01InMilliseconds) / 1000;
+const dateObjWSortAndLong = (dateObj) => {
+    const monthStrings = [
+        ['jan.', 'január'],
+        ['feb.', 'február'],
+        ['már.', 'március'],
+        ['ápr.', 'április'],
+        ['máj.', 'május'],
+        ['jún.', 'június'],
+        ['júl.', 'július'],
+        ['aug.', 'augusztus'],
+        ['szep.', 'szeptember'],
+        ['okt.', 'október'],
+        ['nov.', 'november'],
+        ['dec.', 'december'],
+    ];
+
+    const month = dateObj.getMonth();
+    console.log(month);
+    const shortMonthName = monthStrings[dateObj.getMonth()][0];
+    const longMonthName = monthStrings[dateObj.getMonth()][1];
+
+    const dateStringHU = dateObj.toLocaleDateString('hu');
+    const timeStringHU = dateObj.toLocaleTimeString('hu');
+    let shortTimeStringHU = timeStringHU.slice(0, timeStringHU.length - 3);
+
+    let shortDateStringHU = dateStringHU.replace(`. ${month + 1}.`, `. ${shortMonthName}`);
+    let longDateStringHU = dateStringHU.replace(`. ${month + 1}.`, `. ${longMonthName}`);
+    // shortDateStringHU.length = shortDateStringHU.length - 4;
+
+    let short = `${shortDateStringHU} ${shortTimeStringHU}`;
+    let long = `${longDateStringHU} ${timeStringHU}`;
+
+    return { short: short, long: long };
 }
-console.log('másodpercek jan 1-je óta: ', secondsFromThisYearJan01());
-console.log('eltelt napok jan 1-je óta: ', secondsFromThisYearJan01() / 3600 / 24);
+console.log(dateObjWSortAndLong(new Date()));
