ee8a3b0f860101c98d0d3ddeaff66bd61b1522a2
petern1953 <petern1953@gmail.com>
2020-11-30 14:09:09 +0100

    F01 ok

---
+++ F01.js
@@ -0,0 +1,13 @@
+// Írj egy függvényt, aminek a visszatérési értéke az adott év 01.01.óta eltelt másodperceinek a száma!
+
+const secondsFromThisYearJan01 = () => {
+    const now = new Date();
+    const thisYear = now.getFullYear();
+    const thisMonth = now.getMonth();
+    const thisDay = now.getDate();
+    const jan01InMilliseconds = Date.UTC(thisYear, 0, 1);
+    const nowInMilliseconds = Date.UTC(thisYear, thisMonth, thisDay);
+    return (nowInMilliseconds - jan01InMilliseconds) / 1000;
+}
+console.log('másodpercek jan 1-je óta: ', secondsFromThisYearJan01());
+console.log('eltelt napok jan 1-je óta: ', secondsFromThisYearJan01() / 3600 / 24);
